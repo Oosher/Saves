0282abfad260af3ed71fc8bde7c0faf5a4a73040
Oosher <105375761+Oosher@users.noreply.github.com>
2022-07-08 23:29:58 +0300

    added button site that makes you go to a different site when you press it

---
+++ JavaSript/HWJS/javaScriptLinks.js
@@ -0,0 +1,13 @@
+function youTube(){
+    location.href="https://www.youtube.com/";
+}
+
+
+function faceBook(){
+    location.href="https://www.facebook.com/";
+}
+
+
+function InstaGram(){
+    location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+}
