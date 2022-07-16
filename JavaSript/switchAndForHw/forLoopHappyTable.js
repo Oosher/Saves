adffbb089ed4346258c9cf9844a97ad67620e61e
Oosher <105375761+Oosher@users.noreply.github.com>
2022-07-16 23:45:41 +0300

    RMOVED SOM JUNK CODED

--- JavaSript/switchAndForHw/forLoopHappyTable.js
+++ JavaSript/switchAndForHw/forLoopHappyTable.js
@@ -1,6 +1,5 @@
 function generateTable(){
     var row =document.getElementById("row").value,col=document.getElementById("col").value;
-  document.write(col);
 document.write('<table border="2">');
     for (let i = 0; i < row ; i++) {
         document.write('<tr>');
