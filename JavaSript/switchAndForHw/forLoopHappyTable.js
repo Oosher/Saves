8c5a37f43e33964ceaa4232c44a96f48c40fed2e
Oosher <105375761+Oosher@users.noreply.github.com>
2022-07-17 17:54:29 +0300

    added some switch case and optimized for loop

--- JavaSript/switchAndForHw/forLoopHappyTable.js
+++ JavaSript/switchAndForHw/forLoopHappyTable.js
@@ -3,7 +3,7 @@ function generateTable(){
 document.write('<table border="2">');
     for (let i = 0; i < row ; i++) {
         document.write('<tr>');
-        for (let i = 0; i < col ; i++){
+        for (let j = 0; j < col ; j++){
         document.write('<td>Happy</td>');   
         }
       document.write('</tr>');
