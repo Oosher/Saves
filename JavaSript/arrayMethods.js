8537b3e76d0f8145d2920b70bc4db19a026ed511
Oosher <105375761+Oosher@users.noreply.github.com>
2022-07-27 18:26:40 +0300

    added some array methods

---
+++ JavaSript/arrayMethods.js
@@ -0,0 +1,18 @@
+let arr=[];
+arr[9]=80;
+/* console.log(arr.length); */
+arr[1]=200;
+arr[3]=6000;
+console.log(arr);
+let num=[200,400,600];
+//push()
+num.push(233,3234,5323);
+num[num.length]=4545;
+//unshift();
+num.unshift(4345);
+//pop()
+let lastNum =num.pop();
+//shift()
+num.shift();
+console.log(num);
+console.log(lastNum);
